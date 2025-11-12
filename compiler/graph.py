9b4ce2e947088c849b3d9526347b0c14b858fef8
happycat <xuanhernnn@gmail.com>
2025-11-12 09:17:49 +0800

    Update graph.py

--- compiler/graph.py
+++ compiler/graph.py
@@ -1,26 +1,29 @@
 from graphviz import Digraph
 
+#   visual diagram of syntax tree
 def build_graph(node, graph=None, parent=None):
     if graph is None:
         graph = Digraph()
-
     node_id = str(id(node))
+    # set label for node
     label = node['type']
     if node['type'] == 'id' or node['type'] == 'num':
         label += f": {node['value']}"
-    elif node['type'] == 'binop':
+    elif node['type'] == 'op':
         label += f" ({node['op']})"
 
+    # add node with label to graph
     graph.node(node_id, label)
 
+    # create edge from parent to node if parent exists
     if parent:
         graph.edge(parent, node_id)
 
-    # children
-    if node['type'] == 'stmt':
+    # children nodes
+    if node['type'] == 'assignment':
         for child in node['children']:
             build_graph(child, graph, node_id)
-    elif node['type'] == 'binop':
+    elif node['type'] == 'op':
         build_graph(node['left'], graph, node_id)
         build_graph(node['right'], graph, node_id)
 
